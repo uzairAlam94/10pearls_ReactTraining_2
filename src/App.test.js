d7f26541341976f88b7e121efb80b61a2706a2b1
uzairAlam94 <uzair.badar@tenpearls.com>
2020-12-05 09:03:44 +0500

    Added test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react' 
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+function sum(a,b) { return a+ b; }
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+
+// describe('grouping test', () => {
+//   test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+//   });
+//   test('adds 1 + 2 to equal 3', () => {
+//     expect(typeof(sum)).toBe("function");
+//   });
+// })
+
+
+ it("App renders", () => {
+   render(<App />);
+ });
+
+
+//  screen.debug();
+//  screen.getByText('TodoList');
+//  expect(screen.getByText('TodoList')).toBeInTheDocument();
+// expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+// getByText
+// getByRole
+// getByLabelText
+// getByPlaceholderText
+// getByAltText
+// getByDisplayValue
+
+//Query 
+
+//queryByText
+
+// Find
+
+// findAllBy
+
+//Assertive Functions
+
+/*
+toBeDisabled
+toBeEnabled
+toBeEmpty
+toBeEmptyDOMElement
+toBeInTheDocument
+toBeInvalid
+toBeRequired
+toBeValid
+toBeVisible
+toContainElement
+toContainHTML
+toHaveAttribute
+toHaveClass
+toHaveFocus
+toHaveFormValues
+toHaveStyle
+toHaveTextContent
+toHaveValue
+toHaveDisplayValue
+toBeChecked
+toBePartiallyChecked
+toHaveDescription
+*/
